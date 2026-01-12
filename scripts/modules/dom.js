e83d815fd3b9892bf5b1125ffaaaae63fd5afe7f
Anik Sikder <aniksikder9501@gmail.com>
2026-01-13 02:12:36 +0600

    Create table row for show expense list

---
+++ scripts/modules/dom.js
@@ -0,0 +1,34 @@
+// Create new row in expense table
+export function createRow(expenseData) {
+    console.log(expenseData);
+    const expenseTableBody = document.getElementById('expense-table-body');
+    const tr = document.createElement('tr');
+    if (expenseData?.length === 0) {
+        tr.innerHTML = `<td colspan="4">No expenses added yet.</td>`;
+    } else {
+        tr.innerHTML = `
+            <td>${expenseData?.description}</td>
+            <td>${expenseData?.amount.toFixed(2)}</td>
+            <td>${expenseData?.date}</td>
+            <td><button type="button" class="outline secondary">Delete</button></td>
+            `
+    }
+
+    expenseTableBody.prepend(tr);
+}
+
+// invalidate form input
+export function invalidateInput(inputElement) {
+
+    if (isNaN(inputElement.value) || inputElement.value < 0) {
+        inputElement.setAttribute('aria-invalid', 'true')
+        inputElement.setAttribute('aria-describedby', 'invalid-helper')
+        document.getElementById('invalid-helper').style.display = 'block';
+        return false;
+    } else {
+        inputElement.removeAttribute('aria-invalid');
+        inputElement.removeAttribute('aria-describedby');
+        document.getElementById('invalid-helper').style.display = 'none';
+        return true;
+    }
+}
